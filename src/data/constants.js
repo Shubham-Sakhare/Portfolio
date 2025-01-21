e22b77ad9816fdefba175ce0fcc8cd0ff0afecf2
Shubham-Sakhare <sakhareshubham588@gmail.com>
2025-01-21 18:50:00 +0530

    grade change

--- src/data/constants.js
+++ src/data/constants.js
@@ -79,7 +79,7 @@ export const education = [
     img: swami1,
     collage: "Swami ramananada teerth marathwada university, Nanded",
     date: "Jul 2018 - Oct 2020",
-    grade: 8.0,
+    grade: "A",
     degree: "Msc, Computer Science",
   },
   {
@@ -87,7 +87,7 @@ export const education = [
     img: yeshwantCollege,
     collage: "Yeshwant Collage college Nanded",
     date: "Jul 2015 - May 2018",
-    grade: 9.0,
+    grade: "A",
     degree: "Bsc, Computer Science",
   },
 ];
